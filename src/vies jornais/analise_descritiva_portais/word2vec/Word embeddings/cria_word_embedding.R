04cd419fff03d643ebafc62b8e07aafb49e6cfd0
Allan Sales <allan.sales.89@gmail.com>
2017-11-28 14:05:22 -0300

    separando codigo do artido do codigo de criacao dos word embeddings

---
+++ src/vies jornais/analise_descritiva_portais/word2vec/Word embeddings/cria_word_embedding.R
@@ -0,0 +1,19 @@
+cria_word_embedding <- function(tema, n_layers = 300, analogias, noticias, path_saida){
+  train_file <- paste(path_saida,tema,".csv",sep="")
+  binary_file <- paste(path_saida,tema,".bin",sep="")
+  noticias %>% select(conteudo_processado) %>% write_csv(train_file)
+  modelo <- train_word2vec(train_file, threads = 8, vectors = n_layers)
+  
+  ### Regras que devem ser acertadas para validar embedding
+  analogias = verifica_analogias(binary_file, analogias, respostas)
+  
+  ### Cria tsv com embedding
+  path_modelo_tsv = paste(path_saida,tema,".tsv",sep="")
+  modelo@.Data %>% as.data.frame() %>% write_tsv(path_modelo_tsv)
+  
+  path_modelo_names_tsv = paste(path_saida,tema,"_names",".tsv",sep="")
+  modelo@.Data %>% rownames() %>% as.data.frame() %>% write_tsv(path_modelo_names_tsv)
+  
+  return(list(modelo, analogias))
+}
+
