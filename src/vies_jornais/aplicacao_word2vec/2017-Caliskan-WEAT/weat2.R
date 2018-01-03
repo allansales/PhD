13996715426621a0cf19cf1ab828b2cf46005f96
allansales <allan.sales.89@gmail.com>
2018-01-03 15:36:38 -0200

    continuacao do refatoramenteo de weat

--- src/vies_jornais/aplicacao_word2vec/2017-Caliskan-WEAT/weat2.R
+++ src/vies_jornais/aplicacao_word2vec/2017-Caliskan-WEAT/weat2.R
@@ -1,3 +1,9 @@
+library("dplyr")
+library("wordVectors")
+library("partitions")
+library("gtools")
+library("purrr")
+
 cosSim_model <- function(w1, w2, modelo){
   cosineSimilarity(modelo[[w1]],modelo[[w2]]) %>% as.numeric()
 }
@@ -16,8 +22,10 @@ similarity_table <- function(x, y, a, b, modelo){
     mutate(cos_sim = cosSim_model(target, feature, modelo))
 }
 
-permutacao_table = permutacao(x, y)
-
 get_sim_by_target_feature <- function(sim_table, w_target, w_feature){
   sim_table[sim_table$target == w_target & sim_table$feature == w_feature, "cos_sim"] %>% as.numeric()
 }
+
+sim_table = similarity_table(x, y, a, b, folha_up_down)
+
+permutacao_table = permutacao(x, y)
