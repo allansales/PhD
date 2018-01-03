056191640714aa9dc1d787a5d9eda85f727c95d0
Allan Sales <allan.sales.89@gmail.com>
2018-01-03 18:45:07 -0300

    calculo dos scores para cada grupo pronto

--- src/vies_jornais/aplicacao_word2vec/2017-Caliskan-WEAT/weat2.R
+++ src/vies_jornais/aplicacao_word2vec/2017-Caliskan-WEAT/weat2.R
@@ -17,15 +17,59 @@ similarity_table <- function(x, y, a, b, modelo){
   pares$target = pares$target %>% as.character()
   pares$feature = pares$feature %>% as.character()
   
-  pares %>%
+  pares = pares %>%
     rowwise() %>% 
     mutate(cos_sim = cosSim_model(target, feature, modelo))
+  
+  targets_a = pares %>% filter(feature %in% a)
+  targets_b = pares %>% filter(feature %in% b)
+  return(list(targets_a = targets_a, targets_b = targets_b))
 }
 
-get_sim_by_target_feature <- function(sim_table, w_target, w_feature){
-  sim_table[sim_table$target == w_target & sim_table$feature == w_feature, "cos_sim"] %>% as.numeric()
+permutacao <- function(x, y){
+  
+  remove_repeated_sets = function(Xi, Yi){
+    duplicados = duplicated(Yi)
+    Xi = Xi[!duplicados,]
+    Yi = Yi[!duplicados,]
+    return(list(Xi = Xi, Yi = Yi))
+  }
+  
+  all_targets = c(x,y)
+  n = length(all_targets)
+  Xi = permutations(n=n, r=n/2, v=all_targets) %>% as.data.frame()
+  colnames(Xi) = paste("X",colnames(Xi),sep = "")
+  
+  Yi = Xi %>% apply(1, FUN=function(x){
+    setdiff(all_targets, x)
+  }) %>% t() %>% as.data.frame()
+  
+  colnames(Yi) = paste("Y",colnames(Yi),sep = "")
+  
+  return(remove_repeated_sets(Xi, Yi))
 }
 
-sim_table = similarity_table(x, y, a, b, folha_up_down)
-
+sim_table = similarity_table(x, y, a, b, modelo)
 permutacao_table = permutacao(x, y)
+
+targets_a = sim_table$targets_a
+targets_b = sim_table$targets_b
+
+w_sim_a = targets_a %>% group_by(target) %>% summarise(mean_cos = mean(cos_sim)) %>% arrange(target)
+w_sim_b = targets_b %>% group_by(target) %>% summarise(mean_cos = mean(cos_sim)) %>% arrange(target)
+
+w_dif_a_b = bind_cols(w_sim_a %>% select(target), #poderia usar w_sim_b. as duas terao a mesma ordem
+                      (w_sim_a %>% select(mean_cos) - 
+                       w_sim_b %>% select(mean_cos)))
+
+sum_w_sim <- function(words_set, w_dif_a_b){
+  w_dif_a_b %>% filter(target %in% words_set) %>% summarise(sum_sim = sum(mean_cos)) %>% as.numeric()
+}
+
+score_Xi = permutacao_table$Xi %>% apply(1, sum_w_sim, w_dif_a_b)
+#score_Xi = permutacao_table$Xi %>% rowwise() %>% mutate(score = sum_w_sim(.,w_dif_a_b))
+score_Yi = permutacao_table$Yi %>% apply(1, sum_w_sim, w_dif_a_b)
+#score_Yi = permutacao_table$Yi %>% rowwise() %>% mutate(score = sum_w_sim(.,w_dif_a_b))
+score_Xi_Yi = score_Xi - score_Yi
+
+permutation_scores = bind_cols(permutacao_table$Xi, permutacao_table$Yi, score = score_Xi_Yi)
