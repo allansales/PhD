86755e80a0dace6742efd9fb728512c70fda6a7c
allansales <allan.sales.89@gmail.com>
2018-01-02 22:22:07 -0200

    iniciando melhora de desempenho

---
+++ src/vies_jornais/aplicacao_word2vec/2017-Caliskan-WEAT/weat2.R
@@ -0,0 +1,23 @@
+cosSim_model <- function(w1, w2, modelo){
+  cosineSimilarity(modelo[[w1]],modelo[[w2]]) %>% as.numeric()
+}
+
+similarity_table <- function(x, y, a, b, modelo){
+  targets = c(x, y)
+  features = c(a, b)
+  
+  pares = expand.grid(target = targets, feature = features)
+  
+  pares$target = pares$target %>% as.character()
+  pares$feature = pares$feature %>% as.character()
+  
+  pares %>%
+    rowwise() %>% 
+    mutate(cos_sim = cosSim_model(target, feature, modelo))
+}
+
+permutacao_table = permutacao(x, y)
+
+get_sim_by_target_feature <- function(sim_table, w_target, w_feature){
+  sim_table[sim_table$target == w_target & sim_table$feature == w_feature, "cos_sim"] %>% as.numeric()
+}
